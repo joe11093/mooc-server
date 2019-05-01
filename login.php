687b4f734b6517a1d68c2efbdc4b12baec54a8df
joe11093 <joe11093@users.noreply.github.com>
2019-05-01 16:59:55 +0200

    script for the login process

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/db_connect.php';
+$db = new DB_CONNECT();
+
+//read input
+$body = file_get_contents('php://input');
+//obtain associative array from the json
+$obj = json_decode($body, true);
+$email = $obj["email"];
+$password = $obj["password"];
+
+//execute query
+$sqlquery = "SELECT * FROM utilisateur WHERE utilisateur.email = '$email';";
+$result = mysql_query($sqlquery) or die(mysql_error());
+$row = mysql_fetch_assoc($result);
+
+//check query result
+if($row){
+   if(strcmp($password, $row["password"]) == 0){    //strcmp -> 0 if equal
+   		//make array of results to return
+   		$response = array("id"=>$row["id"], "firstname" => $row["firstname"], "email"=>$row["email"], "type"=>$row["type"]);
+   		echo json_encode($response);
+   }
+   else{
+   	echo "error";
+   }
+}
+else{
+	echo "error";
+}
+?>
