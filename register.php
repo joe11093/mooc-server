2257b6a2a057e19713978f4daa98bb757183e30d
joe11093 <joe11093@users.noreply.github.com>
2019-05-01 16:59:41 +0200

    registration script

--- register.php
+++ register.php
@@ -3,14 +3,29 @@ require_once __DIR__ . '/db_connect.php';
 
 $db = new DB_CONNECT();
 
-/*
-$u_id = (int) $_POST["id"];
-$u_firstname = $_POST["firstname"];
 
-$sqlquery = "INSERT INTO test ('id', 'firstname') VALUES('$u_id', '$u_firstname');";
+//$u_id = (int) $_POST["id"];
+//$u_firstname = $_POST["firstname"];
+
+//$sqlquery = "INSERT INTO test ('id', 'firstname') VALUES('$u_id', '$u_firstname');";
+//$result = mysql_query($sqlquery) or die(mysql_error());
+echo "hi";
+
+/*
+$firstname = $obj["firstname"];
+$lastname = $obj["lastname"];
+$dob = $obj["dob"];
+$email = $obj["email"];
+$password = $obj["password"];
+$city = $obj["city"];
+$country = $obj["country"];
+*/
+
+/*
+$sqlquery = "INSERT INTO utilisateur (firstname, lastname, dateofbirth, email, password, city, country, type) VALUES ('$firstname', '$lastname', '$dob', '$email', '$password', '$city', '$country', 'parent');";
+
 $result = mysql_query($sqlquery) or die(mysql_error());
 */
 
-$result = mysql_query("INSERT INTO test VALUES(112, 'jjk');") or die(mysql_error());
 
 ?>
