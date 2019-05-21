d61d52457f30f79eaa61606793c614ccd6bb7aee
joe11093 <joe11093@users.noreply.github.com>
2019-05-21 03:18:36 +0200

    script to get all DB rows of a certain type

---
+++ get_all_of_type.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . '/db_connect.php';
+
+$db = new DB_CONNECT();
+
+//read input
+$body = file_get_contents('php://input');
+//obtain associative array from the json
+$obj = json_decode($body, true);
+
+$type = $obj["type"];
+if($type == "matiere" || $type == "annee_scolaire"){
+	//query database for table matiere
+	$sqlquery = "SELECT * FROM $type;";
+	$result = mysql_query($sqlquery) or die(mysql_error());
+	if(mysql_num_rows($result) > 0){
+		//there are results 
+		//encode results into json
+		//echo the result
+		$res_array = array();
+		while ($row=mysql_fetch_assoc($result)){
+    		array_push($res_array, $row);
+   		}
+   		echo json_encode($res_array);
+	}
+		else{
+		//no results
+		echo "no_results";
+	}
+	}
+	
+else{
+	echo "error";
+}
+
+?>
